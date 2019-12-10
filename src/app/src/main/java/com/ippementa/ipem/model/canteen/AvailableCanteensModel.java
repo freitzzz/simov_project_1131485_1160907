26471655ea6071da5adfcdf7d25a5f57509403bb
Joao Freitas <1160907@isep.ipp.pt>
2019-12-10 00:04:01 +0000

    Added model for available canteens functionality response payload
    see #6

---
+++ src/app/src/main/java/com/ippementa/ipem/model/canteen/AvailableCanteensModel.java
@@ -0,0 +1,14 @@
+package com.ippementa.ipem.model.canteen;
+
+import java.util.ArrayList;
+
+public class AvailableCanteensModel extends ArrayList<AvailableCanteensModel.Item> {
+
+    public class Item {
+
+        public long id;
+
+        public String name;
+
+    }
+}
