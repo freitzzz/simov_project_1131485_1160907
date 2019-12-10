b87724cde8e311d99351e095d2172efb9c490803
Joao Freitas <1160907@isep.ipp.pt>
2019-12-10 00:08:01 +0000

    Added temporarly code to request available schools in MainActivity

--- src/app/src/main/java/com/ippementa/ipem/MainActivity.java
+++ src/app/src/main/java/com/ippementa/ipem/MainActivity.java
@@ -2,13 +2,56 @@ package com.ippementa.ipem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import com.ippementa.ipem.model.school.AvailableSchoolsModel;
+import com.ippementa.ipem.model.school.SchoolsRepository;
+import com.ippementa.ipem.view.school.AvailableSchoolsActivity;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        new RequestAvailableSchoolsTask().execute();
+    }
+
+    private class RequestAvailableSchoolsTask extends AsyncTask<Void, Integer, AvailableSchoolsModel>{
+
+        @Override
+        protected AvailableSchoolsModel doInBackground(Void... voids) {
+
+            SchoolsRepository repository = new SchoolsRepository();
+
+            try {
+                return repository.availableSchools();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(AvailableSchoolsModel items) {
+            super.onPostExecute(items);
+
+            Intent availableSchoolsActivity = new Intent(MainActivity.this, AvailableSchoolsActivity.class);
+
+            ArrayList<String[]> schools = new ArrayList<>();
+
+            for(AvailableSchoolsModel.Item item : items){
+                schools.add(new String[]{Long.toString(item.id),item.acronym, item.name});
+            }
+
+            availableSchoolsActivity.putExtra("schools", schools);
+
+            startActivity(availableSchoolsActivity);
+        }
     }
 }
