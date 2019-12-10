442bf50b52af3ff7dee1f8fb0b658e361d9ee7de
Joao Freitas <1160907@isep.ipp.pt>
2019-12-10 22:59:45 +0000

    Added view interface for views that can show errors
    see #4

---
+++ src/app/src/main/java/com/ippementa/ipem/view/error/ErrorView.java
@@ -0,0 +1,11 @@
+package com.ippementa.ipem.view.error;
+
+public interface ErrorView {
+
+    void showNoInternetConnectionError();
+
+    void showServerNotAvailableError();
+
+    void showUnexepectedServerFailureError();
+
+}
