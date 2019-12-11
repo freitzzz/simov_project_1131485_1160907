c2899ec59f9fa0f9fcfbdf495770e1d2f9c79224
Joao Freitas <1160907@isep.ipp.pt>
2019-12-11 22:19:18 +0000

    Updated android manifest to include existing activies

--- src/app/src/main/java/com/ippementa/ipem/MainActivity.java
+++ src/app/src/main/java/com/ippementa/ipem/MainActivity.java
@@ -1,17 +1,11 @@
 package com.ippementa.ipem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
-import com.ippementa.ipem.model.school.AvailableSchoolsModel;
-import com.ippementa.ipem.model.school.SchoolsRepository;
 import com.ippementa.ipem.view.school.AvailableSchoolsActivity;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,39 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new RequestAvailableSchoolsTask().execute();
-    }
+        Intent intent = new Intent(this, AvailableSchoolsActivity.class);
 
-    private class RequestAvailableSchoolsTask extends AsyncTask<Void, Integer, AvailableSchoolsModel>{
-
-        @Override
-        protected AvailableSchoolsModel doInBackground(Void... voids) {
-
-            SchoolsRepository repository = new SchoolsRepository();
-
-            try {
-                return repository.availableSchools();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(AvailableSchoolsModel items) {
-            super.onPostExecute(items);
-
-            Intent availableSchoolsActivity = new Intent(MainActivity.this, AvailableSchoolsActivity.class);
-
-            ArrayList<String[]> schools = new ArrayList<>();
-
-            for(AvailableSchoolsModel.Item item : items){
-                schools.add(new String[]{Long.toString(item.id),item.acronym, item.name});
-            }
-
-            availableSchoolsActivity.putExtra("schools", schools);
-
-            startActivity(availableSchoolsActivity);
-        }
+        startActivity(intent);
     }
 }
