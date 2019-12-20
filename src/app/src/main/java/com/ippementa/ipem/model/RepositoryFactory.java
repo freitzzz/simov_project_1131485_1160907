df29e65f526c206494a32270a476921ba7cda8ba
Joao Freitas <1160907@isep.ipp.pt>
2019-12-20 03:18:36 +0000

    Updated room repository factory implementation due to room not initializating database
    see #11

--- src/app/src/main/java/com/ippementa/ipem/model/RepositoryFactory.java
+++ src/app/src/main/java/com/ippementa/ipem/model/RepositoryFactory.java
@@ -1,14 +1,23 @@
 package com.ippementa.ipem.model;
 
+import com.ippementa.ipem.model.canteen.Canteen;
 import com.ippementa.ipem.model.canteen.CanteensRepository;
 import com.ippementa.ipem.model.canteen.IPEDCanteensRepositoryImpl;
+import com.ippementa.ipem.model.canteen.RoomCanteensRepositoryImpl;
+import com.ippementa.ipem.model.dish.Dish;
 import com.ippementa.ipem.model.dish.DishRepository;
 import com.ippementa.ipem.model.dish.IPEDDishRepositoryImpl;
+import com.ippementa.ipem.model.dish.RoomDishRepositoryImpl;
 import com.ippementa.ipem.model.menu.IPEDMenusRepositoryImpl;
+import com.ippementa.ipem.model.menu.Menu;
 import com.ippementa.ipem.model.menu.MenusRepository;
+import com.ippementa.ipem.model.menu.RoomMenusRepositoryImpl;
 import com.ippementa.ipem.model.school.IPEDSchoolsRepositoryImpl;
+import com.ippementa.ipem.model.school.RoomSchoolsRepositoryImpl;
+import com.ippementa.ipem.model.school.School;
 import com.ippementa.ipem.model.school.SchoolsRepository;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 public interface RepositoryFactory {
@@ -48,7 +57,16 @@ public interface RepositoryFactory {
         }
     }
 
-    abstract class RoomRepositoryFactoryImpl extends RoomDatabase implements RepositoryFactory {
+    @Database(entities = {School.class, Canteen.class, Menu.class, Dish.class},version = 1,exportSchema = false)
+    public abstract class RoomRepositoryFactoryImpl extends RoomDatabase implements RepositoryFactory {
+
+        public abstract RoomSchoolsRepositoryImpl createSchoolsRepository();
+
+        public abstract RoomCanteensRepositoryImpl createCanteensRepository();
+
+        public abstract RoomMenusRepositoryImpl createMenusRepository();
+
+        public abstract RoomDishRepositoryImpl createDishRepository();
 
     }
 
