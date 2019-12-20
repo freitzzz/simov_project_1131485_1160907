fa35b7ab6c3fa7cfdfaf1a0cbe7762dd94bba586
Joao Freitas <1160907@isep.ipp.pt>
2019-12-20 00:59:21 +0000

    Provider now provides access to RepositoryFactory instance
    see #11

--- src/app/src/main/java/com/ippementa/ipem/util/Provider.java
+++ src/app/src/main/java/com/ippementa/ipem/util/Provider.java
@@ -1,6 +1,10 @@
 package com.ippementa.ipem.util;
 
+import android.content.Context;
+
 import com.ippementa.ipem.model.RepositoryFactory;
+
+import androidx.room.Room;
 
 /**
  * An utility class that has the responsibility of providing a set of features
@@ -9,13 +13,28 @@ public class Provider {
 
     private static Provider _instance;
 
-    private Provider(){}
+    private Settings settings;
 
-    public static Provider instance(){
+    private RepositoryFactory.RoomRepositoryFactoryImpl roomRepositoryFactory;
+
+    private Provider(Context ctx){
+
+        this.settings = new Settings(ctx);
+
+        if(this.settings.isInOfflineMode()){
+
+            this.roomRepositoryFactory
+                    = Room.databaseBuilder(ctx, RepositoryFactory.RoomRepositoryFactoryImpl.class, "ipp-ementa-sqlite")
+                    .build();
+        }
+
+    }
+
+    public static Provider instance(Context ctx){
 
         if(_instance == null){
 
-            _instance = new Provider();
+            _instance = new Provider(ctx);
 
         }
 
@@ -23,9 +42,23 @@ public class Provider {
 
     }
 
-    public RepositoryFactory repositoryFactory(){
+    public RepositoryFactory repositoryFactory(Context ctx){
 
-        return new RepositoryFactory.IPEDRepositoryFactoryImpl();
+        if(this.settings.isInOfflineMode()){
+
+            if(roomRepositoryFactory == null){
+                this.roomRepositoryFactory
+                        = Room.databaseBuilder(ctx, RepositoryFactory.RoomRepositoryFactoryImpl.class, "ipp-ementa-sqlite")
+                        .build();
+            }
+
+            return roomRepositoryFactory;
+
+        }else{
+
+            return new RepositoryFactory.IPEDRepositoryFactoryImpl();
+
+        }
 
     }
 
