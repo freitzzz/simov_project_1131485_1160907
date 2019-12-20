073868f20bb7a109d164bb1270c493ca8a3cc5b5
Pedro Coelho [1131485] <1131485@isep.ipp.pt>
2019-12-20 00:29:44 +0000

    See #9 Reimplemented the user preferences screen

--- src/app/src/main/java/com/ippementa/ipem/MainActivity.java
+++ src/app/src/main/java/com/ippementa/ipem/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Intent intent = new Intent(this, AvailableSchoolsActivity.class);
-        Intent intent = new Intent(this, UserPreferencesActivity.class);
+        Intent intent = new Intent(this, AvailableSchoolsActivity.class);
+        //Intent intent = new Intent(this, UserPreferencesActivity.class);
         startActivity(intent);
     }
 }
