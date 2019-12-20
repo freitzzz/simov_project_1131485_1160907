0d8318350d039898d238b9d4efa583329c6d34fe
Joao Freitas <1160907@isep.ipp.pt>
2019-12-20 00:53:43 +0000

    Added class that manages the settings in which the user can opt in
    see #4

---
+++ src/app/src/main/java/com/ippementa/ipem/util/Settings.java
@@ -0,0 +1,66 @@
+package com.ippementa.ipem.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Manages the settings in which the user can opt in
+ */
+public class Settings {
+
+    private boolean cachedInOfflineMode;
+
+    protected Settings(Context ctx){
+
+        reloadSettings(ctx);
+
+    }
+
+    public boolean activateOfflineMode(Context ctx){
+
+        SharedPreferences preferences
+                = ctx.getSharedPreferences("ipp-ementa-shared-preferences", Context.MODE_PRIVATE);
+
+        boolean writtenWithSuccess = preferences.edit().putBoolean("in-offline-mode", true).commit();
+
+        if(!writtenWithSuccess){
+            return false;
+        }else{
+            reloadSettings(ctx);
+            return true;
+        }
+    }
+
+    public boolean deactivateOfflineMode(Context ctx){
+
+        SharedPreferences preferences
+                = ctx.getSharedPreferences("ipp-ementa-shared-preferences", Context.MODE_PRIVATE);
+
+        boolean writtenWithSuccess = preferences.edit().putBoolean("in-offline-mode", false).commit();
+
+        if(!writtenWithSuccess){
+            return false;
+        }else{
+            reloadSettings(ctx);
+            return true;
+        }
+    }
+
+    public boolean isInOfflineMode(){
+
+        return cachedInOfflineMode;
+
+    }
+
+    private void reloadSettings(Context ctx){
+
+        SharedPreferences preferences
+                = ctx.getSharedPreferences("ipp-ementa-shared-preferences", Context.MODE_PRIVATE);
+
+        boolean offlineMode = preferences.getBoolean("in-offline-mode", false);
+
+        this.cachedInOfflineMode = offlineMode;
+
+    }
+
+}
