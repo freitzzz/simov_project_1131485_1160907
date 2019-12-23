f7614888c27d60bac44798deea94aae5457e9d61
Joao Freitas <1160907@isep.ipp.pt>
2019-12-23 20:52:46 +0000

    Fixed application build due to unexisting class reference

--- src/app/src/main/java/com/ippementa/ipem/MainActivity.java
+++ src/app/src/main/java/com/ippementa/ipem/MainActivity.java
@@ -3,7 +3,6 @@ package com.ippementa.ipem;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.ippementa.ipem.view.preferences.UserPreferencesActivity;
 import com.ippementa.ipem.view.school.AvailableSchoolsActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Intent intent = new Intent(this, AvailableSchoolsActivity.class);
-        //Intent intent = new Intent(this, UserPreferencesActivity.class);
+
         startActivity(intent);
     }
 }
