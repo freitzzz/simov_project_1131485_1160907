2abd9315d950c10970d07eb7805c2ccb6eb5e134
Joao Freitas <1160907@isep.ipp.pt>
2020-01-05 04:14:50 +0000

    MainActivity now grants that all files are existent in internal storage, or else finish application
    see #4

--- src/app/src/main/java/com/ippementa/ipem/MainActivity.java
+++ src/app/src/main/java/com/ippementa/ipem/MainActivity.java
@@ -12,14 +12,44 @@ import com.google.firebase.iid.InstanceIdResult;
 import com.ippementa.ipem.util.Provider;
 import com.ippementa.ipem.view.school.AvailableSchoolsActivity;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String[] INTERNAL_STORAGE_FILES = new String[]{
+            "porto.map",
+            "edges",
+            "geometry",
+            "location_index",
+            "nodes",
+            "nodes_ch_fastest_car_node",
+            "properties",
+            "shortcuts_fastest_car_node",
+            "string_index_keys",
+            "string_index_vals"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            grantThatAllFilesExistInInternalStorage();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            // If an IOException occurs then not all required files exist in the internal storage
+            // This means that the application state is not valid to continue usage
+
+            finish();
+        }
 
         FirebaseInstanceId.getInstance().getInstanceId()
                 .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
@@ -44,5 +74,38 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, AvailableSchoolsActivity.class);
 
         startActivity(intent);
+    }
+
+    private void grantThatAllFilesExistInInternalStorage() throws IOException{
+
+        for(String file : INTERNAL_STORAGE_FILES) {
+
+            File fileOnInternalStorage = new File(getExternalFilesDir(null), file);
+
+            if(!fileOnInternalStorage.exists()) {
+
+                FileOutputStream out = new FileOutputStream(fileOnInternalStorage);
+
+                System.out.println(file);
+
+                int resourceId = getResources().getIdentifier(file.split("\\.")[0], "raw", getPackageName());
+
+                System.out.println(resourceId);
+
+                copyStream(getResources().openRawResource(resourceId), out);
+
+                out.close();
+            }
+
+        }
+
+    }
+
+    private static void copyStream(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
     }
 }
