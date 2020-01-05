1888c06a9db447f8b9d130d807eb5c92cc5bb960
Pedro Coelho [1131485] <1131485@isep.ipp.pt>
2020-01-05 16:31:21 +0000

    See #18 Refractoring

---
+++ src/app/src/main/java/com/ippementa/ipem/view/dish/PurchaseDishNFCActivity.java
@@ -0,0 +1,13 @@
+package com.ippementa.ipem.view.dish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PurchaseDishNFCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_dish_nfc);
+    }
+}
